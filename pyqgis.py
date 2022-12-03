37a1ddb51bcac2cc2333037b1eb15d1c5ee4237d
salahelfarissi <salaheddineelfarissi@gmail.com>
2022-12-03 18:34:22 +0100

    Calculate ellipsoidal distance

--- pyqgis.py
+++ pyqgis.py
@@ -1,4 +1,17 @@
-layer = iface.activeLayer()
-layer.startEditing()
-layer.deleteAttribute(1)
-layer.commitChanges()
+from qgis.core import QgsDistanceArea, QgsPointXY
+
+# Calculate the ellipsoidal distance between two points
+taza = (34.2106, 3.9980)
+rabat = (33.9716, 6.8498)
+
+d = QgsDistanceArea()
+d.setEllipsoid('WGS84')
+
+lat1, lon1 = taza
+lat2, lon2 = rabat
+
+point1 = QgsPointXY(lon1, lat1)
+point2 = QgsPointXY(lon2, lat2)
+
+d = d.measureLine([point1, point2])
+print(d/1000)
