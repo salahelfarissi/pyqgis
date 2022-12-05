48012c4586ead2b777fdb1fce9e5ee6d32271ed7
salahelfarissi <salaheddineelfarissi@gmail.com>
2022-12-05 11:40:58 +0100

    Add countries as basemap

--- birds.py
+++ birds.py
@@ -1,8 +1,12 @@
 from qgis.core import QgsVectorLayer, QgsProject
 
 # Import csv layer
-url = "file:///C:/Users/Salah/Downloads/birds.csv?delimiter=%s&xField=%s&yField=%s&crs=epsg:4326" % (",", "location-long", "location-lat")
+url = "file:///C:/Users/Salah/Documents/GitHub/pyqgis/data/birds.csv?delimiter=%s&xField=%s&yField=%s&crs=epsg:4326" % (",", "location-long", "location-lat")
 csvLayer = QgsVectorLayer(url, "Birds", "delimitedtext")
 
-# Add layer to the map
+# Add countries layer
+countriesLayer = QgsVectorLayer("C:/Users/Salah/Documents/GitHub/pyqgis/data/countries/countries.shp", "Countries", "ogr")
+
+# Add layers to the map
+QgsProject.instance().addMapLayer(countriesLayer)
 QgsProject.instance().addMapLayer(csvLayer)
