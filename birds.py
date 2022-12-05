46f1743dc090baa3b37f2c704b34f530cd0a3918
salahelfarissi <salaheddineelfarissi@gmail.com>
2022-12-05 16:23:58 +0100

    Set a filter on a dataset

--- birds.py
+++ birds.py
@@ -1,12 +1,37 @@
 from qgis.core import QgsVectorLayer, QgsProject
 
 # Import csv layer
-url = "file:///C:/Users/Salah/Documents/GitHub/pyqgis/data/birds.csv?delimiter=%s&xField=%s&yField=%s&crs=epsg:4326" % (",", "location-long", "location-lat")
+url = "file:///C:/Users/Salah/Documents/GitHub/pyqgis/data/black-tailed.csv?delimiter=%s&xField=%s&yField=%s&crs=epsg:4326" % (",", "location-long", "location-lat")
 csvLayer = QgsVectorLayer(url, "Birds", "delimitedtext")
 
 # Add countries layer
-countriesLayer = QgsVectorLayer("C:/Users/Salah/Documents/GitHub/pyqgis/data/countries/countries.shp", "Countries", "ogr")
+# countriesLayer = QgsVectorLayer("C:/Users/Salah/Documents/GitHub/pyqgis/data/countries/countries.shp", "Countries", "ogr")
+
+QgsProject.instance().removeAllMapLayers()
 
 # Add layers to the map
-QgsProject.instance().addMapLayer(countriesLayer)
-QgsProject.instance().addMapLayer(csvLayer)
+# QgsProject.instance().addMapLayer(countriesLayer)
+# QgsProject.instance().addMapLayer(csvLayer)
+
+# Construct an iterator to iterate over the features of the layer
+iterator = csvLayer.getFeatures()
+
+NAMES = []
+for feature in iterator:
+    featureAttributes = feature.attributes()
+    birdName = featureAttributes[26]
+
+    if birdName not in NAMES:
+        NAMES.append(birdName)
+
+NAMES.sort()
+print(NAMES)
+
+# field_names = [field.name() for field in csvLayer.fields()]
+# for field in field_names:
+#     print(field)
+
+# Define a subset of the layer
+csvLayer.setSubsetString("\"individual-local-identifier\" = 'Amalia'")
+for feature in csvLayer.getFeatures():
+    print(feature["individual-local-identifier"])
