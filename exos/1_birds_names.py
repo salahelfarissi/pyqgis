6949e359a239c490a2d946908a1baaa049b39499
salahelfarissi <salaheddineelfarissi@gmail.com>
2023-01-20 17:50:04 +0100

    Move bird names script to exos folder

---
+++ exos/1_birds_names.py
@@ -0,0 +1,27 @@
+from qgis.core import QgsVectorLayer, QgsProject
+import os
+
+# Import csv layer
+url = "file:///C:/Users/Salah/Documents/GitHub/pyqgis/data/black-tailed.csv?delimiter=%s&xField=%s&yField=%s&crs=epsg:%s" % (",", "location-long", "location-lat", 4326)
+csvLayer = QgsVectorLayer(url, "Birds", "delimitedtext")
+
+# Add countries layer
+countriesLayer = QgsVectorLayer("C:/Users/Salah/Documents/GitHub/pyqgis/data/countries/countries.shp", "Countries", "ogr")
+
+# Remove all layers from the map
+QgsProject.instance().removeAllMapLayers()
+
+# Add layers to the map
+QgsProject.instance().addMapLayer(countriesLayer)
+QgsProject.instance().addMapLayer(csvLayer)
+
+# Construct an iterator to iterate over the features of the layer
+iterator = csvLayer.getFeatures()
+
+# Get list of bird names
+featureAttributes = [feature.attributes() for feature in iterator]
+NAMES = [featureAttributes[i][26] for i in range(len(featureAttributes))]
+NAMES = sorted(list(set(NAMES)))
+
+# Define a subset of the layer
+csvLayer.setSubsetString("\"individual-local-identifier\" = 'Amalia'")
